5ab33df63b55e32c102b14069cad57af06876767
yessgm <yessicagaonam@gmail.com>
2023-11-19 14:47:53 -0600

    Initial Commit

---
+++ src/main/java/com/tictactoe/model/TicTacToeBoard.java
@@ -0,0 +1,75 @@
+package com.tictactoe.model;
+
+public class TicTacToeBoard {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToeBoard() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = ' ';
+            }
+        }
+    }
+
+    public boolean makeMove(int row, int col) {
+        if (isValidMove(row, col)) {
+            board[row][col] = currentPlayer;
+            togglePlayer();
+            return true;
+        }
+        return false;
+    }
+
+    private void togglePlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public char getWinner() {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return board[i][0];  // Row win
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+                return board[0][i];  // Column win
+            }
+        }
+
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return board[0][0];  // Diagonal win (top-left to bottom-right)
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return board[0][2];  // Diagonal win (top-right to bottom-left)
+        }
+
+        return ' ';  // No winner yet
+    }
+
+    public boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == ' ') {
+                    return false;  // The board is not full
+                }
+            }
+        }
+        return true;  // The board is full (draw)
+    }
+}
+
